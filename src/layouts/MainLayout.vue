4ca4d0404949897c59ef3c15d0766835e2ae543b
Denis412 <d-a-malyshev@yandex,ru>
2023-03-22 15:58:05 +0300

    Инициализация проекта и чистка от дефолтных компонентов, ненужных переменных и т.д.

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-layout view="hHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Список пользователей </q-item-label>
+      </q-list>
+    </q-drawer>
+
+    <q-drawer v-model="rightDrawerOpen" side="right" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Список пользователей </q-item-label>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view v-slot="{ Component }">
+        <keep-aliveq>
+          <component :is="Component" />
+        </keep-aliveq>
+      </router-view>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const leftDrawerOpen = ref(false);
+const rightDrawerOpen = ref(false);
+
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+};
+
+const toggleRightDrawer = () => {
+  rightDrawerOpen.value = !rightDrawerOpen.value;
+};
+</script>
