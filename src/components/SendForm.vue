d58837440650f89937f651deeee3763ad62dc504
Denis412 <d-a-malyshev@yandex,ru>
2023-03-23 23:56:35 +0300

    Исправлена форма ввода сообщения (теперь она на всю ширину пространства чата)

--- src/components/SendForm.vue
+++ src/components/SendForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-form class="form-send flex items-center">
+  <q-form class="flex items-center">
     <div class="form-controls q-ml-md">
       <q-input type="text" v-model="message" placeholder="Ваше сообщение..." />
     </div>
@@ -56,3 +56,9 @@ const sendMessage = async () => {
   }
 };
 </script>
+
+<style scoped>
+.form-controls {
+  flex-grow: 2;
+}
+</style>
