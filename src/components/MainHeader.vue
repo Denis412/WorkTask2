ce0e542d90ced592eab2e6061a3eda64cdce26f6
Denis412 <d-a-malyshev@yandex,ru>
2023-03-23 14:03:45 +0300

    Добавлен тогглер для показа/скрытия правого меню со списком пользователей

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -10,7 +10,7 @@
         @click="toggleLeftDrawer"
       />
 
-      <div>
+      <div class="flex items-center">
         <q-item id="auth-links">
           <q-btn @click="userApi.login"> Войти </q-btn>
         </q-item>
@@ -18,6 +18,15 @@
         <div class="q-ml-md">
           <div id="user-button"></div>
         </div>
+        <q-btn
+          class="burger_btn"
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleRightDrawer"
+        />
       </div>
     </q-toolbar>
   </q-header>
@@ -28,4 +37,17 @@ import { inject } from "vue";
 import userApi from "../sdk/user";
 
 const toggleLeftDrawer = inject("toggleLeftDrawer");
+const toggleRightDrawer = inject("toggleRightDrawer");
 </script>
+
+<style>
+.burger_btn {
+  display: none;
+}
+
+@media (max-width: 1006px) {
+  .burger_btn {
+    display: flex;
+  }
+}
+</style>
